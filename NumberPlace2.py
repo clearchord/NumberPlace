340eac2099a98a257461ea7276a9827b38a0631e
A. O <4520050+clearchord@users.noreply.github.com>
2021-02-08 22:30:25 +0900

    Update NumberPlace2.py

--- NumberPlace2.py
+++ NumberPlace2.py
@@ -1,7 +1,6 @@
-from os import pardir
-from typing import List
+import sys
+from typing import List, Tuple, Set
 import copy
-import pdb
 
 class Cell:
     def __init__(self, row: int, column: int):
@@ -12,16 +11,16 @@ class Cell:
         self.number = -1
         self.excluded = [False] * 9
 
-    def fix(self, number):
+    def fix(self, number: int):
         self.number = number
 
-    def exclude(self, number):
+    def exclude(self, number: int):
         self.excluded[number] = True
 
-    def remaining(self):
+    def remaining(self) -> int:
         return 0 if 0 <= self.number else self.excluded.count(False)
 
-    def get_candidates(self):
+    def get_candidates(self) -> List[int]:
         candidates = []
         if self.number < 0:
             for index, fixed in enumerate(self.excluded):
@@ -29,7 +28,7 @@ class Cell:
                     candidates.append(index)
         return candidates
 
-    def determinable(self):
+    def determinable(self) -> Tuple[bool, int]:
         result = (False, -1)
         if self.number < 0:
             candidates = self.get_candidates()
@@ -58,35 +57,24 @@ class Board:
         propagate the information to its row, column, and block.
         '''
         self.cells[row][column].fix(number)
+        # For code simplicity, it is OK to perform exclude() on cell (row, column) 3 times.
         for r0 in range(0, 9):
-            if r0 == row:
-                continue
-            else:
-                self.cells[r0][column].exclude(number)
+            self.cells[r0][column].exclude(number)
         for c0 in range(0, 9):
-            if c0 == column:
-                continue
-            else:
-                self.cells[row][c0].exclude(number)
+            self.cells[row][c0].exclude(number)
         block_row = (row // 3) * 3
         block_column = (column // 3) * 3
         for r in range(0, 3):
             br = block_row + r
             for c in range(0, 3):
                 bc = block_column + c
-                if br == row and bc == column:
-                    continue
-                else:
-                    self.cells[br][bc].exclude(number)
+                self.cells[br][bc].exclude(number)
 
-    def load(self, matrix: List[str]):
-        for row in range(0, 9):
-            for column in range(0, 9):
-                number = int(matrix[row][column])
-                if 0 < number:
-                    self.fix(row, column, number - 1)
+    def apply(self, determined: Set[Tuple[int, int, int]]):
+        for row, column, number in determined:
+            self.fix(row, column, number)
 
-    def determine(self):
+    def determine(self) -> Set[Tuple[int, int, int]]:
         newly_determined = set()
         for row in range(0, 9):
             for column in range(0, 9):
@@ -95,53 +83,74 @@ class Board:
                     newly_determined.add((row, column, number))
         return newly_determined
 
-    def from_file(self, path_problem):
-        with open(path_problem, 'r', encoding='utf-8') as file:
-            example = file.readlines()
-        return example
+    def satulate(self, determined: Set[Tuple[int, int, int]]={}):
+        if 0 < len(determined):
+            self.apply(determined)
+        next_determined = self.determine()
+        while 0 < len(next_determined):
+            self.apply(next_determined)
+            next_determined = self.determine()
 
-    def find_splittable(self):
+    def find_splittable(self) -> Tuple[int, int, List[int]]:
+        splittable = None
         for row in range(0, 9):
             for column in range(0, 9):
                 candidates = self.cells[row][column].get_candidates()
                 if 1 < len(candidates):
-                    return (row, column, candidates)
-        return (-1, -1, None)
+                    splittable = (row, column, candidates)
+                    break
+            if splittable:
+                break
+        return (-1, -1, None) if splittable is None else splittable
+    
+    def is_successful(self) -> bool:
+        successful = True
+        for row in range(0, 9):
+            for column in range(0, 9):
+                if self.cells[row][column].number == -1:
+                    successful = False
+                    break
+            if not successful:
+                break
+        return successful
 
-if __name__ == '__main__':
-    board = Board()
-    problem = board.from_file('problem149.txt')
-    board.load(problem)
-    next_determined = board.determine()
-    print()
-    while 0 < len(next_determined):
-        for row, column, number in next_determined:
-            board.fix(row, column, number)
-        next_determined = board.determine()
-        #board.show()
-    #board.show_remainings()
 
-    current_boards = [board]
+def load_problem(path_problem: str) -> Set[Tuple[int, int, int]]:
+    determined = set()
+    with open(path_problem, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+    for row in range(0, 9):
+        for column in range(0, 9):
+            number = int(lines[row][column])
+            if 0 < number:
+                determined.add((row, column, number - 1))
+    return determined
+
+def main(path_problem):
     finished_boards = set()
-
+    board = Board()
+    next_determined = load_problem(path_problem)
+    board.satulate(next_determined)
+    current_boards = [board]
     while 0 < len(current_boards):
         current_board = current_boards.pop()
         row, column, candidates = current_board.find_splittable()
         if 0 <= row:
             for candidate in candidates:
                 cloned_board = copy.deepcopy(current_board)
-                cloned_board.fix(row, column, candidate)
-                next_determined = cloned_board.determine()
-                while 0 < len(next_determined):
-                    for r, c, number in next_determined:
-                        cloned_board.fix(r, c, number)
-                    next_determined = cloned_board.determine()
+                cloned_board.satulate([(row, column, candidate)])
                 current_boards.append(cloned_board)
         else:
-            finished_boards.add(current_board)
+            if current_board.is_successful():
+                finished_boards.add(current_board)
 
     print('--solutions')
     for board in finished_boards:
         board.show()
     print(f'# of solutions: {len(finished_boards)}')
 
+if __name__ == '__main__':
+    path_problem = 'problem149.txt'
+    if 1 < len(sys.argv):
+        path_problem = sys.argv[1]
+    main(path_problem)
